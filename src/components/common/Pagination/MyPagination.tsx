14e275103c9a6b96954aa1e0707706d7ee4a0d7d
benyamin <benyamin.84.yt@gmail.com>
2025-02-04 11:21:39 +0330

    add animation

--- src/components/common/Pagination/MyPagination.tsx
+++ src/components/common/Pagination/MyPagination.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { usePagination, PaginationItemType, cn } from "@heroui/react";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function MyPagination({
   itemCount,
@@ -36,33 +37,40 @@ export default function MyPagination({
             return <li key={page + String(index)}>...</li>;
 
           return (
-            <li
-              key={page}
-              aria-label={`page ${page}`}
-              className="w-4 h-4 flex items-center"
-            >
-              {activePage === page ? (
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 14 11"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  onClick={() => setPage(page)}
-                  className="cursor-pointer -translate-x-0.5"
-                >
-                  <path
-                    d="M7.73205 0.999999C6.96225 -0.333334 5.03775 -0.333332 4.26795 1L0.803847 7C0.0340471 8.33334 0.996299 10 2.5359 10L9.4641 10C11.0037 10 11.966 8.33333 11.1962 7L7.73205 0.999999Z"
-                    fill="#8cff45"
-                  />
-                </svg>
-              ) : (
-                <button
-                  className="w-1/2 h-1/2 bg-[#D9D9D9] rounded-full"
-                  onClick={() => setPage(page)}
+            <motion.li
+            key={page}
+            aria-label={`page ${page}`}
+            className="w-4 h-4 flex items-center"
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {activePage === page ? (
+              <motion.svg
+                width="16"
+                height="16"
+                viewBox="0 0 12 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setPage(page)}
+                className="cursor-pointer"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <path
+                  d="M7.73205 0.999999C6.96225 -0.333334 5.03775 -0.333332 4.26795 1L0.803847 7C0.0340471 8.33334 0.996299 10 2.5359 10L9.4641 10C11.0037 10 11.966 8.33333 11.1962 7L7.73205 0.999999Z"
+                  fill="#8cff45"
                 />
-              )}
-            </li>
+              </motion.svg>
+            ) : (
+              <motion.button
+                className="w-1/2 h-1/2 bg-[#D9D9D9] rounded-full"
+                onClick={() => setPage(page)}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              />
+            )}
+          </motion.li>
           );
         })}
       </ul>
