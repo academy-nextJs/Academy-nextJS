3bc9ff8ecdf308fed7c1e5fc685454fd4be55ca3
benyamin <benyamin.84.yt@gmail.com>
2025-02-04 16:11:05 +0330

    CONFIG FONTS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Hero from "@/components/landing/hero/Hero";
 export default function Home() {
   return (
     <div className="text-gray border-2 bg-base border-gray space-y-8">
-     <p> HomePage </p> 
+     <p className="font-Peyda-900"> سلام </p> 
       <Hero />
     </div>
   );
