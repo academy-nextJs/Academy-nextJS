ea75f329f61aa858f4fd4430e588c73f35c97345
sobhan369arab <sobhanarab30@gmail.com>
2025-02-07 00:48:46 +0330

    complete file container component

--- src/components/common/FileContainer/index.tsx
+++ src/components/common/FileContainer/index.tsx
@@ -2,15 +2,15 @@
 import { FC, ReactNode, useRef } from "react";
 
 enum RADIUS_SIZE {
-  "sm" = "[14px]",
-  "md" = "3xl",
-  "lg" = "[40px]",
+  "sm" = "14px",
+  "md" = "24px",
+  "lg" = "40px",
 }
 
 interface ClassNames {
   base: string;
   label: string;
-  cover: string;
+  labelHeight: string;
   tag: string;
 }
 
@@ -19,8 +19,8 @@ interface FileContainerProps {
   classNames?: Partial<ClassNames>;
   background: string;
   children: ReactNode;
-  tag: ReactNode;
-  label: ReactNode;
+  tag?: ReactNode;
+  label?: ReactNode;
 }
 
 const FileContainer: FC<FileContainerProps> = ({
@@ -35,25 +35,38 @@ const FileContainer: FC<FileContainerProps> = ({
 
   return (
     <div
-      className={`w-[306px] h-[203px] relative rounded-tr-none 
-        mt-10 ${classNames?.base} rounded-${RADIUS_SIZE?.[radius]} bg-[${background}]`}
+      className={`w-[306px] h-[203px] relative
+        mt-10 ${classNames?.base}`}
+      style={{
+        borderRadius: RADIUS_SIZE?.[radius],
+        borderTopRightRadius: "0",
+        backgroundColor: background,
+      }}
     >
       <div
         ref={labelRef}
-        className={`w-[140px] h-10 absolute z-10 -top-10 right-0 
-          ${classNames?.label} rounded-t-${RADIUS_SIZE?.[radius]} bg-[${background}]`}
+        className={`w-[140px] absolute z-10 right-0
+          ${classNames?.label}`}
+        style={{
+          borderTopRightRadius: RADIUS_SIZE?.[radius],
+          borderTopLeftRadius: RADIUS_SIZE?.[radius],
+          backgroundColor: background,
+          height: classNames?.labelHeight,
+          top: `-${classNames?.labelHeight} `,
+        }}
       >
         {label}
         <div
-          className={`w-5 h-5 rounded-br-full absolute bottom-0 
-            -left-5 shadow-[6px_6px] ${classNames?.cover} shadow-[${background}]`}
+          className={`w-5 h-5 rounded-br-full absolute bottom-0
+            -left-5`}
+          style={{ boxShadow: `6px 6px  ${background}` }}
         ></div>
       </div>
       <div
         className={`w-[calc(100%-${
           labelRef.current?.clientWidth.toString() + "px"
         })]
-          absolute -top-10 left-0 ${classNames?.tag} h-10 bg-transparent`}
+          absolute -top-10 left-0 ${classNames?.tag} h-10`}
       >
         {tag}
       </div>
