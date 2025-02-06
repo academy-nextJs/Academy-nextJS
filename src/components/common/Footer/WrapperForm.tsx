d8b641fd21668efbe869de09e1dec9aad4d44600
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-02-06 22:00:01 +0330

    add triangles icon

--- src/components/common/Footer/WrapperForm.tsx
+++ src/components/common/Footer/WrapperForm.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Form from "./Form";
 
 export default function WrapperForm() {
@@ -6,7 +7,10 @@ export default function WrapperForm() {
       dir="rtl"
       className="font-Peyda-500 max-lg:col-span-2 max-lg:flex flex-wrap justify-center mb-12"
     >
-      <h5 className="max-lg:w-full max-lg:text-center">همیشه کنارتیم</h5>
+      <div className="flex gap-x-4">
+        <h5 className="max-lg:w-full max-lg:text-center">همیشه کنارتیم</h5>
+        <Image width={48} height={16} alt="" src="/three-triangles.svg" />
+      </div>
       <h1 className="text-4xl my-6 max-sm:text-center text-nowrap">
         24 ساعت روز و 7 روز هفته در اختیار شماییم !
       </h1>
