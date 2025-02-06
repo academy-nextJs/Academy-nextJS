ce361a4fed65daeb11a64afcedfcd92c5f070de8
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-02-06 18:55:48 +0330

    create and responsive footer

---
+++ src/components/common/Footer/WrapperForm.tsx
@@ -0,0 +1,20 @@
+import Form from "./Form";
+
+export default function WrapperForm() {
+  return (
+    <div
+      dir="rtl"
+      className="font-Peyda-500 max-lg:col-span-2 max-lg:flex flex-wrap justify-center mb-12"
+    >
+      <h5 className="max-lg:w-full max-lg:text-center">همیشه کنارتیم</h5>
+      <h1 className="text-4xl my-6 max-sm:text-center text-nowrap">
+        24 ساعت روز و 7 روز هفته در اختیار شماییم !
+      </h1>
+      <p className="text-xl font-Peyda-400 xl:text-nowrap max-lg:text-center">
+        تیم دلتا با ارائه بهترین نیرو های خدماتی و سرویس های املاکی سعی دارد تا
+        بتواند در تمام لحظات کنار شما باشد .
+      </p>
+      <Form />
+    </div>
+  );
+}
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,18 +1,13 @@
+import WrapperForm from "./WrapperForm";
 import Poster from "./Poster";
+import WrapperLinks from "./WrapperLinks";
 
 export default function Footer() {
   return (
-    <footer className="w-[1376px] grid grid-cols-2 p-8 pt-16 rounded-3xl bg-[#8CFF45]">
+    <footer className="w-[1376px] h-fit grid grid-cols-2 p-8 pt-16 rounded-3xl bg-[#8CFF45]">
       <Poster />
-      <div className="bg-red-500"></div>
-      <div dir="rtl" className="bg-[#232323] p-[48px] min-h-[496px] rounded-[56px] col-span-2 relative">
-        <h1 className="text-white text-[32px]">پلتفرم دلتا</h1>
-        <div className="w-full h-[180px] bg-red-200"></div>
-        <div className="w-full h-[56px] bg-red-500"></div>
-        <div className="w-full h-[56px] bg-red-500"></div>
-        <hr />
-        <div className="w-full h-[40px] bg-red-900"></div>
-      </div>
+      <WrapperForm />
+      <WrapperLinks />
     </footer>
   );
 }
