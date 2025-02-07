374088776cbf32402fe289f44bfcf7bcaf90cae3
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-02-07 11:38:21 +0330

    fix bugs footer

--- src/components/common/FileContainer/index.tsx
+++ src/components/common/FileContainer/index.tsx
@@ -11,6 +11,7 @@ interface ClassNames {
   base: string;
   label: string;
   labelHeight: string;
+  labelWidth: string;
   tag: string;
 }
 
@@ -35,12 +36,12 @@ const FileContainer: FC<FileContainerProps> = ({
 
   return (
     <div
-      className={`w-[306px] h-[203px] relative
-        mt-10 ${classNames?.base}`}
+      className={`w-[306px] h-[203px] relative ${classNames?.base}`}
       style={{
         borderRadius: RADIUS_SIZE?.[radius],
-        borderTopRightRadius: "0",
+        borderTopRightRadius: "0px",
         backgroundColor: background,
+        marginTop: classNames?.labelHeight
       }}
     >
       <div
@@ -49,18 +50,29 @@ const FileContainer: FC<FileContainerProps> = ({
           ${classNames?.label}`}
         style={{
           borderTopRightRadius: RADIUS_SIZE?.[radius],
-          borderTopLeftRadius: RADIUS_SIZE?.[radius],
+          borderTopLeftRadius: "100%",
           backgroundColor: background,
           height: classNames?.labelHeight,
+          width: classNames?.labelWidth,
           top: `-${classNames?.labelHeight} `,
         }}
       >
         {label}
         <div
-          className={`w-5 h-5 rounded-br-full absolute bottom-0
-            -left-5`}
-          style={{ boxShadow: `6px 6px  ${background}` }}
-        ></div>
+          className="skew-x-[-45deg] absolute -left-6 bottom-0"
+          style={{
+            backgroundColor: background,
+            height: classNames?.labelHeight,
+            width: `calc(${classNames?.labelWidth} - 10%)`,
+            borderTopLeftRadius: RADIUS_SIZE?.[radius],
+          }}
+        >
+          <div
+            className={`w-5 h-5 rounded-br-full z-10 absolute bottom-0
+            left-[-19px]`}
+            style={{ boxShadow: `4px 6px ${background}` }}
+          ></div>
+        </div>
       </div>
       <div
         className={`w-[calc(100%-${
