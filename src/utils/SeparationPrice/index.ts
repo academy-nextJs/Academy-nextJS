e5dab86b707fb8416bb09c93525c8c4b0afc6765
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-02-08 22:37:46 +0330

    add function for separation price

---
+++ src/utils/SeparationPrice/index.ts
@@ -0,0 +1,13 @@
+export const UnitPrice = (price: number) => {
+  const reducer = Intl.NumberFormat("fa", {
+    notation: "compact",
+  });
+  const newPrice = reducer.format(price);
+  return newPrice;
+};
+
+export const SeparationPrice = (price: string) => {
+  price = String(price);
+  const CategorizedPrice = price.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return CategorizedPrice;
+};
