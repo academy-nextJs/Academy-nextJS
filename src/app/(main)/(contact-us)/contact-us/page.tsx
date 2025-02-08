ae0502b2801dc01997611f3bc86001de11c86de2
sobhan369arab <sobhanarab30@gmail.com>
2025-02-08 22:12:18 +0330

    create contactUs component

--- src/app/(main)/(contact-us)/contact-us/page.tsx
+++ src/app/(main)/(contact-us)/contact-us/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import NextBreadcrumb from "@/components/common/BreadCrumb";
+import ContactUsWrapper from "@/components/contact-us";
+import React from "react";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <>
+      <NextBreadcrumb pageElement={"ارتباط با ما"} />
+      <ContactUsWrapper />
+    </>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
---
+++ src/components/contact-us/index.tsx
@@ -0,0 +1,10 @@
+
+const ContactUsWrapper = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactUsWrapper
