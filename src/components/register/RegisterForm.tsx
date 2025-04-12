02ae799f9f53bc0c9549673c81bdc427b3a9c278
BenyaminHossein-zadeh <benyaminhosseinzadeh84@gmail.com>
2025-04-12 16:12:54 +0330

    register form

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -9,78 +9,104 @@ import Link from "next/link";
 import { BiCheck } from "react-icons/bi";
 
 const RegisterForm = () => {
-    // Handle Formik
-    const formik = useFormik({
-        initialValues: {
-            PhoneNumber: "",
-            Email: "",
-        },
-        onSubmit: (value) => {
-            console.log(value);
-        },
-
-    });
-    console.log(formik.values);
-    const [isRegisterWithNumber, setIsRegisterWithNumber] = useState<boolean>(true);
-    console.log(isRegisterWithNumber);
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <div className="flex gap-2 justify-between items-center">
-                <Input
-                    className="max-md:w-full w-[47%]"
-                    label="شماره تماس:"
-                    placeholder="مثال : 09116918744"
-                    variant="bordered"
-                    classNames={{
-                        label: `label-input px-2 !top-0 !bg-base`,
-                    }}
-                    id="Email"
-                    name="PhoneNumber"
-                    value={formik.values.PhoneNumber}
-                    onChange={formik.handleChange}
-                    disabled={!isRegisterWithNumber}
-                    endContent={
-                        <>
-                            <input className="hidden peer" id="isRegisterwithNumberRadio" type="radio" radioGroup="zz" onChange={() => setIsRegisterWithNumber(!isRegisterWithNumber)} checked={isRegisterWithNumber} />
-                            <label className="cursor-pointer border border-gray size-5 rounded-[4px] peer-checked:bg-purple" htmlFor="isRegisterwithNumberRadio">{isRegisterWithNumber && <BiCheck />}</label>
-                        </>
-                    }
-                />
-                یا
-                <Input
-                    className="max-md:w-full w-[47%]"
-                    type="email"
-                    label="ایمیل شما:"
-                    placeholder="مثال : dakjsbd@email.com"
-                    variant="bordered"
-                    classNames={{
-                        label: `label-input px-2 !top-0 !bg-base`,
-                    }}
-                    id="Email"
-                    name="Email"
-                    value={formik.values.Email}
-                    onChange={formik.handleChange}
-                    disabled={isRegisterWithNumber}
-                    endContent={
-                        <>
-                            <input className="hidden peer" id="isRegisterwithEmailRadio" type="radio" radioGroup="zz" onChange={() => setIsRegisterWithNumber(!isRegisterWithNumber)} checked={!isRegisterWithNumber} />
-                            <label className=" cursor-pointer size-5 border border-gray rounded-[4px] peer-checked:bg-purple " htmlFor="isRegisterwithEmailRadio">{!isRegisterWithNumber && <BiCheck />}</label>
-                        </>
-
-                    }
-                />
-            </div>
-            <Button
-                color="green"
-                size="full"
-                height="26px"
-                className="!py-5 gap-3 !text-[#232323] mt-[100px] !rounded-xl"
-                endContent={<MdKeyboardArrowLeft size={18} />}
+  // Handle Formik
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+    },
+    onSubmit: (value) => {
+      console.log(value);
+    },
+  });
+  // visible password
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className="flex flex-wrap gap-8 items-center">
+        <Input
+          className="max-md:w-full w-[calc(50%_-_16px)]"
+          type="email"
+          label="ایمیل شما:"
+          // labelPlacement="outside"
+          placeholder="مثال : dakjsbd@email.com"
+          variant="bordered"
+          classNames={{
+            label: `label-input px-2 !top-0 !bg-base`,
+          }}
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <Input
+          className="max-md:w-full w-[calc(50%_-_16px)]"
+          label="کلمه عبور:"
+          endContent={
+            <button
+              aria-label=""
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
             >
-                ورود به حساب کاربری
-            </Button>
-        </form>
-    );
+              {isVisible ? <PiEyeLight size={27} /> : <PiEyeSlash size={27} />}
+            </button>
+          }
+          classNames={{
+            label: `label-input px-2 !-top-2 !bg-base`,
+          }}
+          // labelPlacement="outside"
+          type={isVisible ? "text" : "password"}
+          variant="bordered"
+          id="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+
+        <Input
+          className="max-md:w-full w-[calc(50%_-_16px)]"
+          label="نام شما:"
+          // labelPlacement="outside"
+          placeholder="بنیامین"
+          variant="bordered"
+          classNames={{
+            label: `label-input px-2 !top-0 !bg-base`,
+          }}
+          id="firstName"
+          name="firstName"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+        />
+        <Input
+          className="max-md:w-full w-[calc(50%_-_16px)]"
+          label="نام خانوادگی شما:"
+          // labelPlacement="outside"
+          placeholder="حسین زاده"
+          variant="bordered"
+          classNames={{
+            label: `label-input px-2 !top-0 !bg-base`,
+          }}
+          id="lastName"
+          name="lastName"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+      </div>
+      <Button
+        color="green"
+        size="full"
+        height="26px"
+        className="!py-5 gap-3 !text-[#232323] mt-[100px] !rounded-xl"
+        endContent={<MdKeyboardArrowLeft size={18} />}
+      >
+        ورود به حساب کاربری
+      </Button>
+    </form>
+  );
 };
 
 export default RegisterForm;
