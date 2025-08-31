81da29b8d466ab17af97f6d9eabecbe4232cf336
Amir Mohammad Molaei <amirmmdmolaei@gmail.com>
2025-08-31 10:31:41 +0330

    fix FileContainer component and add clsx package

--- src/components/common/FileContainer/index.tsx
+++ src/components/common/FileContainer/index.tsx
@@ -1,36 +1,11 @@
 "use client";
-import { FC, ReactNode, useRef } from "react";
+import { FC } from "react";
+import { RadiusSize, ShapeProps } from "./types";
+import Label from "./label";
+import Tag from "./tag";
+import { cn } from "../../../utils/cn";
 
-enum RADIUS_SIZE {
-  "sm" = "14px",
-  "md" = "24px",
-  "lg" = "40px",
-}
-
-interface ClassNames {
-  base: string;
-  innerLabel: string;
-  wrapperLabel: string;
-  wrapperTag: string;
-  coverHollow: string;
-}
-
-export interface FileContainerProps {
-  children: ReactNode;
-  background: string;
-  radius: "sm" | "md" | "lg";
-  size: "lg" | "md";
-  width?: string;
-  height?: string;
-  label?: ReactNode;
-  tag?: ReactNode;
-  labelWidth?: string;
-  labelHeight?: string;
-  tagHeight?: string;
-  classNames?: Partial<ClassNames>;
-}
-
-const FileContainer: FC<FileContainerProps> = ({
+const FileContainer: FC<ShapeProps> = ({
   children,
   background,
   radius,
@@ -44,84 +19,34 @@ const FileContainer: FC<FileContainerProps> = ({
   tagHeight,
   classNames,
 }) => {
-  const labelRef = useRef<HTMLDivElement>(null);
-  const tagRef = useRef<HTMLDivElement>(null);
-
-  // Hollows
-  const HollowSizeMD = () => {
-    return (
-      <div
-        style={{
-          boxShadow: `4px 6px`,
-          color: `${background}`,
-          pointerEvents: "none",
-        }}
-        className={`w-5 h-5 rounded-br-full z-10 absolute bottom-0
-            left-[-19px] ${classNames?.coverHollow}`}
-      ></div>
-    );
-  };
-  const HollowSizeLG = () => {
-    return (
-      <div
-        className="skew-x-[-45deg] absolute -left-6 bottom-0"
-        style={{
-          backgroundColor: background,
-          height: labelHeight,
-          width: `80%`,
-          borderTopLeftRadius: RADIUS_SIZE?.[radius],
-        }}
-      >
-        <div
-          className={`w-5 h-5 rounded-br-full z-10 absolute bottom-0
-            left-[-19px] ${classNames?.coverHollow}`}
-          style={{ boxShadow: `4px 6px ${background}` }}
-        ></div>
-      </div>
-    );
-  };
-
   return (
     <div
-      className={`h-fit relative z-30 ${classNames?.base}`}
+      className={cn("h-fit relative z-30", classNames?.base)}
       style={{
         width: width,
         height: height,
-        borderRadius: RADIUS_SIZE?.[radius],
+        borderRadius: RadiusSize?.[radius],
         borderTopRightRadius: "0px",
         backgroundColor: background,
         marginTop: labelHeight,
       }}
     >
-      <div
-        ref={labelRef}
-        className={`absolute z-30 !right-0 ${classNames?.wrapperLabel}`}
-        style={{
-          borderTopRightRadius: RADIUS_SIZE?.[radius],
-          borderTopLeftRadius: size == "lg" ? "30%" : RADIUS_SIZE?.[radius],
-          backgroundColor: background,
-          height: labelHeight,
-          width: labelWidth,
-          top: `-${labelHeight}`,
-        }}
-      >
-        <div className={`relative z-40 w-full p-4 ${classNames?.innerLabel}`}>
-          {label}
-        </div>
-        {size == "lg" ? <HollowSizeLG /> : <HollowSizeMD />}
-      </div>
-      <div
-        ref={tagRef}
-        className={`absolute !left-0 h-fit flex justify-end items-start ${classNames?.wrapperTag}`}
-        style={{
-          width: `calc(95% - ${labelWidth})`,
-          height: `${tagHeight ? tagHeight : labelHeight}`,
-          top: `-${tagHeight ? tagHeight : labelHeight}`,
-          pointerEvents: "none",
-        }}
-      >
-        {tag}
-      </div>
+      <Label
+        background={background}
+        radius={radius}
+        size={size}
+        classNames={classNames}
+        label={label}
+        labelHeight={labelHeight}
+        labelWidth={labelWidth}
+      />
+      <Tag
+        classNames={classNames}
+        labelHeight={labelHeight}
+        labelWidth={labelWidth}
+        tagHeight={tagHeight}
+        tag={tag}
+      />
       {children}
     </div>
   );
---
+++ src/utils/cn/index.ts
@@ -0,0 +1,4 @@
+import { clsx } from "clsx";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const cn = (...inputs: any[]) => clsx(inputs);
